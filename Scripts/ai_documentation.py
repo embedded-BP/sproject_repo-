951fcfa0f243a140382c74b3dab199ebb74efb9b
BSP <embedded@jsdelectronics.co.in>
2026-06-27 11:22:34 +0530

    script .py file modified

--- Scripts/ai_documentation.py
+++ Scripts/ai_documentation.py
@@ -1,1 +1,11 @@
+#print("AI Documentation Generator Started")
+from pathlib import Path
+
 print("AI Documentation Generator Started")
+
+docs = Path("Docs")
+
+for file in docs.glob("*.md"):
+    print(file.name)
+
+print("Completed")
